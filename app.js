bac9ead76efa6cfcb8d9616957114d0c1469b1bf
sandalu95 <sandalukalpanee@gmail.com>
2019-05-23 19:02:49 +0530

    User signup added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/user");
 
 mongoose.connect('mongodb+srv://node-shop:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-ubenn.mongodb.net/test?retryWrites=true',{
   useNewUrlParser:true
@@ -31,8 +32,10 @@ app.use((req, res, next) => {
   next();
 });
 
+//Routes which should handle requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user",userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
