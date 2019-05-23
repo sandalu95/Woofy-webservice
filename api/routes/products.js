92a56f8a6cf2a67955bda02d7f546d19dbd2e963
sandalu95 <sandalukalpanee@gmail.com>
2019-05-23 10:24:31 +0530

    Image upload enabled

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination:function(req,file,cb){
+    cb(null,'./uploads/');
+  },
+  filename:function(req,file,cb){
+    cb(null,Date.now()+file.originalname);
+  }
+});
+
+const fileFilter = (req,file,cb)=>{
+  //reject a file
+  if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+    cb(null,true);
+  } else {
+    cb(null,false);
+  }
+}
+
+const upload = multer({
+  storage:storage, 
+  limits:{
+    fileSize: 1024*1024*5
+  },
+  fileFilter:fileFilter
+});
 
 const Product = require('../models/product');
 
@@ -40,7 +67,7 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", upload.single('productImage'),(req, res, next) => {
   const product = new Product({
     _id:new mongoose.Types.ObjectId(),
     name:req.body.name,
