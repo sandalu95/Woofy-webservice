e87e692aea1f2febea5c660ce1e71bb588cf6943
sandalu95 <sandalukalpanee@gmail.com>
2019-05-23 10:50:33 +0530

    Image retrieve enabled

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb+srv://node-shop:' + process.env.MONGO_ATLAS_PW + '@nod
 mongoose.Promise = global.Promise;
 
 app.use(morgan("dev"));
+app.use('/uploads',express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
