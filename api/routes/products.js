7c05f6406281e1d6a45084b6eaec1e5a036338d6
sandalu95 <sandalukalpanee@gmail.com>
2019-05-24 12:01:44 +0530

    Added User Controller

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
 const multer = require('multer');
 const checkAuth = require('../middleware/check-auth');
 const ProductsController = require('../controllers/products');
@@ -31,38 +30,13 @@ const upload = multer({
   fileFilter:fileFilter
 });
 
-const Product = require('../models/product');
-
 router.get("/", ProductsController.products_get_all );
 
 router.post("/",checkAuth, upload.single('productImage'),ProductsController.products_create_product);
 
 router.get("/:productId", ProductsController.products_get_product );
 
-router.patch("/:productId", checkAuth, (req, res, next) => {
-  const id = req.params.productId;
-  const updateOps = {};
-  for(const ops of req.body){
-    updateOps[ops.propName]=ops.value;
-  }
-  Product.update({_id:id},{$set:updateOps})
-    .exec()
-    .then(result=>{
-      res.status(200).json({
-        message:'Product updated successfully',
-        request:{
-          type:'GET',
-          url:'http://localhost:3000/products/'+id
-        }
-      });
-    })
-    .catch(err=>{
-      console.log(err);
-      res.status(500).json({
-        error:err
-      });
-    });
-});
+router.patch("/:productId", checkAuth, ProductsController.products_update_product);
 
 router.delete("/:productId", checkAuth,ProductsController.products_delete_product);
 
